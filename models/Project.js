dcbbc5e1e0bb7c98312ad99d63bdabbe06e243b3
Trey <trey.shearin3@gmail.com>
2024-05-23 22:06:20 -0400

    created thought model code

---
+++ models/Project.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const projectSchema = new Schema({
+    name: {
+      type: String,
+      required: 'You need to name the project!',
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    members:[{
+        type:Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+   
+  });
+  
+  const Project = model('Project', ProjectSchema);
+  
+  module.exports = Project;
+  

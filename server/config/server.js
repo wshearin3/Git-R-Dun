97cb8a0df0c858119e49bf99a2782011b74c0045
tabi-led <tabiletallecx@gmail.com>
2024-05-28 20:11:35 -0500

    started commit

---
+++ server/config/server.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+

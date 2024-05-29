60798d0f81ef76ce57ada6e889db087791a93c89
tabi-led <tabiletallecx@gmail.com>
2024-05-28 20:28:47 -0500

    now listening

--- server/config/server.js
+++ server/config/server.js
@@ -2,33 +2,33 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+//const routes = require('./controllers');
+//const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
+//const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-const hbs = exphbs.create({ helpers });
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-      maxAge: 300000,
-      httpOnly: false,
-      secure: false,
-      sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+const hbs = exphbs.create();
+//const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {
+//       maxAge: 300000,
+//       httpOnly: false,
+//       secure: false,
+//       sameSite: 'strict',
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize
+//     })
+//   };
 
-  app.use(session(sess));
+ // app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
@@ -38,8 +38,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 //start point for routes
-app.use(routes);
+//app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+//sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening at http://localhost:3001'));
-});
+//});

d2875f16fc7e7d8669836a9f4e3b3485dceda751
tabi-led <tabiletallecx@gmail.com>
2024-05-28 20:12:17 -0500

    continued

--- server/config/server.js
+++ server/config/server.js
@@ -5,4 +5,11 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+const hbs = exphbs.create({ helpers });

38167ab61470242dda0aa4e121ae0ba0b465ecbc
Joselyn Nuno <nunojosy0420@gmail.com>
2024-06-03 16:42:15 -0500

    edited

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,53 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from './App.jsx';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Tasks from './pages/Tasks';
+import Calendar from './pages/Calendar';
+import Settings from './pages/Settings';
+import Profile from './pages/Profile';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }, {
+                path: '/login',
+                element: <Login />
+            }, {
+                path: '/signup',
+                element: <Signup />
+            }, {
+                path: '/tasks',
+                element: <Tasks />
+            }, {
+                path: '/calendar',
+                element: <Calendar />
+            }, {
+                path: '/settings',
+                element: <Settings />
+            }, {
+                path: '/profiles/:username',
+                element: <Profile />
+            }, {
+                path: '/me',
+                element: <Profile />
+            }
+        ]
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+);

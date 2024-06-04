f0bd2120c9f58dd108cc831f990e1bd6178e7c26
AHuss13 <Adamhuss13@gmail.com>
2024-06-03 21:42:52 -0500

    formatting

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,36 +1,68 @@
-const { User, Project } = require('../models');
+const { User, Project } = require("../models");
 
 const resolvers = {
-    Query: {
-        users: async () => {
-            try {
-                const users = await User.find();
-                return users;
-            } catch (error) {
-                throw new Error('Failed to fetch users');
-            }
-        },
-        projects: async () => {
-            try {
-                const projects = await Project.find();
-                return projects;
-            } catch (error) {
-                throw new Error('Failed to fetch projects');
-            }
-        }
-
+  Query: {
+    users: async () => {
+      try {
+        const users = await User.find();
+        return users;
+      } catch (error) {
+        throw new Error("Failed to fetch users");
+      }
     },
+    projects: async () => {
+      try {
+        const projects = await Project.find();
+        return projects;
+      } catch (error) {
+        throw new Error("Failed to fetch projects");
+      }
+    },
+  },
 
-    Mutation: {
-        addUser: async (parent, { username }) => {
-            const user = await User.create({ username });
-            return { user };
-        },
-        addProject: async (parent, { name, description, owner, members }) => {
-            const project = await Project.create({ name, description, owner, members });
-            console.log(project);
-            return project;
-        }
-    }
-}
-    module.exports = resolvers;
+  Mutation: {
+    addUser: async (parent, { username }) => {
+      const user = await User.create({ username });
+      return { user };
+    },
+    addProject: async (parent, { name, description, owner, members }) => {
+      const project = await Project.create({
+        name,
+        description,
+        owner,
+        members,
+      });
+      console.log(project);
+      return project;
+    },
+    // Copied currently
+    // updateProject: async (parent, { id, name, description, members }) => {
+    //   const project = await Project.findOneAndUpdate({
+    //     id,
+    //     name,
+    //     description,
+    //     members,
+    //   });
+    //   console.log(project);
+    //   return project;
+    // },
+
+    removeProject: async (parent, { projectId }, context) => {
+      // if (context.user)
+      {
+        const project = await Project.findOneAndDelete({
+          _id: projectId
+        });
+
+        // await User.findOneAndUpdate(
+        //   { _id: context.user._id },
+        //   { $pull: { Projects: Project._id } }
+        // );
+        return project;
+      }
+      // throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;

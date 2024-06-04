ff3808939b7362fb257ddb14d70886d3570af2a6
AHuss13 <Adamhuss13@gmail.com>
2024-06-03 21:43:21 -0500

    added remove project

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const {gql} = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = `
 
@@ -25,8 +25,8 @@ const typeDefs = `
   type Mutation{
     addUser(username: String!): User!
     addProject(name: String!, description: String!, owner: ID!, members: [ID]): Project
-  
+    removeProject(projectId: ID!): Project
   } 
   `;
 
-  module.exports = typeDefs;
+module.exports = typeDefs;

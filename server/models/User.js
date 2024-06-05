1c6c2c0a2dbd4cd30d0bfe56cc516fea18c3cb8c
Trey <trey.shearin3@gmail.com>
2024-06-04 21:23:32 -0400

    updated user model and typedef

--- server/models/User.js
+++ server/models/User.js
@@ -8,37 +8,37 @@ const userSchema = new Schema({
     unique: true,
     trim: true,
   },
-  // email: {
-  //   type: String,
-  //   required: true,
-  //   unique: true,
-  //   //match: [/.+@.+\..+/, 'Must match an email address!'],
-  // },
-  // password: {
-  //   type: String,
-  //   required: true,
-  //   minlength: 5,
-  // },
-  // projects: [
-  //   {
-  //     type: Schema.Types.ObjectId,
-  //     ref: 'Project',
-  //   },
-  // ],
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    //match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  projects: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Project',
+    },
+  ],
 });
 
-// userSchema.pre('save', async function (next) {
-//   if (this.isNew || this.isModified('password')) {
-//     const saltRounds = 10;
-//     this.password = await bcrypt.hash(this.password, saltRounds);
-//   }
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-//   next();
-// });
+  next();
+});
 
-// userSchema.methods.isCorrectPassword = async function (password) {
-//   return bcrypt.compare(password, this.password);
-// };
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("User", userSchema);
 

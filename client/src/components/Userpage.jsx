8e87c400475905d765d6385530927dd97325d6fe
Trey <trey.shearin3@gmail.com>
2024-06-06 20:38:09 -0400

    added additional page

--- client/src/components/Userpage.jsx
+++ client/src/components/Userpage.jsx
@@ -1,21 +1,26 @@
 import react, { useContext, useState } from "react";
-import { Heading, Stack, Card, CardHeader, CardBody, CardFooter  } from '@chakra-ui/react'
+import { Heading, Stack, Card, CardHeader, CardBody, CardFooter, Container } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Link } from "react-router-dom";
 
 
 
-function Userpage (){
-    return(
-        
+function Userpage() {
+    return (
+        <>
 
-        <Link to={"/projectpage"}>
-            <Stack spacing={4} direction='row' align='center'>
-            <Button colorScheme='teal' size='sm'>
-    Project 1
-  </Button>
-  </Stack>
-  </Link>
+            <Container centerContent>
+                <Stack spacing={4} direction='row' align='center'>
+                    <Link to={"/projectpage"}>
+
+                        <Button colorScheme='teal' size='sm'>
+                            Project 1
+                        </Button>
+
+                    </Link>
+                </Stack>
+            </Container>
+        </>
     )
 }
 

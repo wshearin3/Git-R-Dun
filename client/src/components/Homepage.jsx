69b19830563c2a197094fa673a304592c56787cb
Trey <trey.shearin3@gmail.com>
2024-06-09 13:50:00 -0400

    added login fields to homepage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import react, { useContext, useState } from "react";
-import { Heading, Stack  } from '@chakra-ui/react'
+import { Heading, Stack, Input  } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Link } from "react-router-dom";
 
@@ -7,9 +7,25 @@ import { Link } from "react-router-dom";
 
 function Homepage (){
     return(
+        <>
+        <Heading size='lg' my="5px">Login</Heading>
+        <Stack spacing={3}>
+                                <Input placeholder='Username' size='sm' />
+                                <Input placeholder='Password' size='sm' />
+                                </Stack>
         <Link to={"/userpage"}>
-            <Button colorScheme='blue'>User Page</Button>
+            <Button colorScheme='blue' my="5px">Login</Button>
   </Link>
+  <Heading size='lg' my="5px">Register</Heading>
+  <Stack spacing={3}>
+                                <Input placeholder='Username' size='sm' />
+                                <Input placeholder='Email' size='sm' />
+                                <Input placeholder='Password' size='sm' />
+                                </Stack>
+  <Link to={"/userpage"}>
+            <Button colorScheme='blue' my="5px">Register</Button>
+  </Link>
+  </>
     )
 }
 
